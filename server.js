e0543627c1859001f70061db5cdd92038c8cf7c3
Caden-Zuehsow <150456584+Caden-Zuehsow@users.noreply.github.com>
2025-11-26 16:29:49 -0500

    Update server.js

--- server.js
+++ server.js
@@ -155,7 +155,7 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log('listening on', PORT));
 
-});
+);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log('listening on', PORT));
